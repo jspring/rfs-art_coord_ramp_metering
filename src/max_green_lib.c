d6871a1a7f0c7b1f38c9006e344bf22f6a24e823
jspring <jspring@path.berkeley.edu>
2013-07-24 23:03:36 -0700

    modified:   max_green_lib.c modified:   max_green_lib.h 	Dongyan added right turn (turn onto southbound ramp) inputs to get_measurement 	and get_new_max_green_phase3 function calls 	Also changed the calculations appropriately

--- src/max_green_lib.c
+++ src/max_green_lib.c
@@ -1,30 +1,31 @@
 #include "max_green_lib.h"
 
 #define MAX_GREEN_UPPERBOUND 25
-#define MAX_GREEN_LOWERBOUND 10
+#define MAX_GREEN_LOWERBOUND 12
 #define LTQUEUE_THRESHOLD 20.0
 #define RAMP_LIMIT 76.0
 #define RAMP_QUEUE_THRESHOLD RAMP_LIMIT/2
-#define RTQUEUE_THRESHOLD 20.0
+//#define RTQUEUE_THRESHOLD 20.0
 #define MAX_GREEN_CHANGE 2
 #define RAMP_QUEUE_DELTA 10.0
+#define RT_RELEASE_THRESHOLD 20
 
 #define bool	unsigned char
 #define true	1
 #define false	0
 
-int get_measurement(FILE *fp, double *LT_queue, double *ramp_queue)
+int get_measurement(FILE *fp, double *LT_queue, double *ramp_queue, double *RT_queue)
 {
 	int flag;
-	flag = fscanf(fp, "%lf %lf", LT_queue, ramp_queue);
-	if(flag!=2)
+	flag = fscanf(fp, "%lf %lf %lf", LT_queue, ramp_queue, RT_queue);
+	if(flag!=3)
 		return 1;
 	else
 		return 0;
 }
 
 
-bool need_reduce_max_green(double LT_queue, double old_LT_queue, double ramp_queue, double old_ramp_queue)
+unsigned char need_reduce_max_green(double LT_queue, double old_LT_queue, double ramp_queue, double old_ramp_queue)
 {
 	
 	if((LT_queue>LTQUEUE_THRESHOLD && LT_queue>old_LT_queue) || (ramp_queue>RAMP_QUEUE_THRESHOLD && ramp_queue>old_ramp_queue-RAMP_QUEUE_DELTA))
@@ -39,31 +40,29 @@ bool need_reduce_max_green(double LT_queue, double old_LT_queue, double ramp_que
 
 
 
-int get_new_max_green_phase3(double LT_queue, double old_LT_queue, double ramp_queue, double old_ramp_queue, int old_max_green)
+int get_new_max_green_phase3(double LT_queue, double old_LT_queue, double ramp_queue, double old_ramp_queue, int old_max_green, double RT_queue, double new_meter_rate, double old_meter_rate)
 {
 	int new_max_green = old_max_green;
 	if(need_reduce_max_green(LT_queue, old_LT_queue, ramp_queue, old_ramp_queue))	//need to reduce time
 	{
-		printf("get_new_max_green_phase3: reducing time; old max green %d ", new_max_green); 
 		new_max_green = old_max_green-MAX_GREEN_CHANGE;
 		new_max_green = max(new_max_green,MAX_GREEN_LOWERBOUND);
-		printf("new max green %d\n", new_max_green); 
 	}
 	else	//can increase time
 	{
-		printf("get_new_max_green_phase3: increasing time; old max green %d ", new_max_green); 
 		new_max_green = old_max_green+MAX_GREEN_CHANGE;
 		new_max_green = min(new_max_green,MAX_GREEN_UPPERBOUND);
-		printf("new_max_green %d\n", new_max_green); 
 	}
-
+	if(RT_queue>RT_RELEASE_THRESHOLD)
+	{
+		new_max_green=MAX_GREEN_UPPERBOUND;
+	}
 	return new_max_green;
 }
 
 int set_new_max_green_phase3(int new_max_green)
 {
-	int flag;
+	int flag=0;
 	printf("%d\n",new_max_green);
-//	scanf("%d",&flag);
 	return flag;
 }
