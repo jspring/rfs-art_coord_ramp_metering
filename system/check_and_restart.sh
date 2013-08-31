8c37b7fe3cf108ec05ef808ad88d419cbba15eb3
John Spring <jspring@berkeley.edu>
2013-08-30 19:24:24 -0700

    modified:   check_and_restart.sh 	Added checks for arterial computer processes

--- system/check_and_restart.sh
+++ system/check_and_restart.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
+ps -elf |grep -v grep | grep '/home/art_coord_ramp_metering/src/lnx/wrfiles_ac_rm'
 
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/artgo
+fi
+
+ps -elf |grep -v grep | grep 'receive_urms_data'
+
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/artgo
+fi
+
+ps -elf |grep -v grep | grep '/home/atsc/ab3418/lnx/ab3418comm'
+
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/urmsgo
+fi
+
+ps -elf |grep -v grep | grep '/home/art_coord_ramp_metering/src/lnx/clt_vars'
+
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/urmsgo
+fi
+
+ps -elf |grep -v grep | grep '/home/path/db/lnx/db_slv'
+
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/urmsgo
+fi
