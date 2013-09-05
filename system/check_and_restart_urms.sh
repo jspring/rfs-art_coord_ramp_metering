8fe4995331a2a7f11d19a371f9bf0ffc92ad0a63
John Spring <jspring@berkeley.edu>
2013-09-05 16:28:20 -0700

    modified:   ../system/check_and_restart_urms.sh 	Checks for any process not running, and restarts everything

--- system/check_and_restart_urms.sh
+++ system/check_and_restart_urms.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
+CTR=0
+
 ps -elf |grep -v grep | grep 'wrfiles_ac_rm'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/urmsgo
+	CTR=1
 fi
 
 ps -elf |grep -v grep | grep 'send_urms_data'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/urmsgo
+	CTR=1
 fi
 
 ps -elf |grep -v grep | grep '/home/atsc/urms/lnx/urms'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/urmsgo
+	CTR=1
 fi
 
 ps -elf |grep -v grep | grep '/home/path/db/lnx/db_slv'
 
 if [[ $? -ne 0 ]]
 then
+	CTR=1
+fi
+
+if [[ $CTR -ne 0 ]]
+then
 	/home/art_coord_ramp_metering/system/urmsgo
 fi
+
+exit 0
