f177ab098e2e7a6d403bcc1af4eb0f4a23f4e548
John Spring <jspring@berkeley.edu>
2013-09-05 13:32:39 -0700

    modified:   check_and_restart_urms.sh 	Added check for wrfiles_ac_rm

--- system/check_and_restart_urms.sh
+++ system/check_and_restart_urms.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ps -elf |grep -v grep | grep 'wrfiles_ac_rm'
+
+if [[ $? -ne 0 ]]
+then
+	/home/art_coord_ramp_metering/system/urmsgo
+fi
+
 ps -elf |grep -v grep | grep 'send_urms_data'
 
 if [[ $? -ne 0 ]]
