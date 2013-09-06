2becf0c6fb70a2053f03aae681267e254d0633f2
John Spring <jspring@berkeley.edu>
2013-09-05 17:09:10 -0700

    modified:   check_and_restart.sh 	Checks all processes, and restarts everything

--- system/check_and_restart.sh
+++ system/check_and_restart.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-ps -elf |grep -v grep | grep '/home/art_coord_ramp_metering/src/lnx/wrfiles_ac_rm'
+CTR=0
+
+ps -elf |grep -v grep | grep 'wrfiles_ac_rm'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/artgo
+	CTR=1
 fi
 
-ps -elf |grep -v grep | grep 'receive_urms_data'
+ps -elf |grep -v grep | grep '/home/atsc/ab3418/lnx/receive_urms_data'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/artgo
+	CTR=2
+fi
+
+ps -elf |grep -v grep | grep '/home/art_coord_ramp_metering/system/receive_urms_data_go.sh'
+
+if [[ $? -ne 0 ]]
+then
+	CTR=3
 fi
 
 ps -elf |grep -v grep | grep '/home/atsc/ab3418/lnx/ab3418comm'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/urmsgo
-fi
-
-ps -elf |grep -v grep | grep '/home/art_coord_ramp_metering/src/lnx/clt_vars'
-
-if [[ $? -ne 0 ]]
-then
-	/home/art_coord_ramp_metering/system/urmsgo
+	CTR=4
 fi
 
 ps -elf |grep -v grep | grep '/home/path/db/lnx/db_slv'
 
 if [[ $? -ne 0 ]]
 then
-	/home/art_coord_ramp_metering/system/urmsgo
+	CTR=5
 fi
+
+if [[ $CTR -ne 0 ]]
+then
+	echo CTR $CTR
+	/home/art_coord_ramp_metering/system/urmsgo
+	exit 1
+fi
+
+exit 0
