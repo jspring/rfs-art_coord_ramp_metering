d79ac4c75d3704b9d04370394b7310a5219c663e
John Spring <jspring@berkeley.edu>
2013-10-21 16:23:44 -0700

    modified:   ../system/send_urms_data_go.sh 	Added sending of packets directly to TSCP laptop over 10. LAN

--- system/send_urms_data_go.sh
+++ system/send_urms_data_go.sh
@@ -4,7 +4,11 @@ while [[ 1 ]]
 do
 	if [[ $1 == "home" ]]
 	then
-		/home/atsc/urms/lnx/send_urms_data -r 10.0.1.5
+		/home/atsc/urms/lnx/send_urms_data -r 10.0.1.202
+
+# Use the following if sobu5 is used, otherwise, send directly to TSCP
+# laptop over 10.x.x.x LAN
+#		/home/atsc/urms/lnx/send_urms_data -r 10.0.1.5
 	else
 		/home/atsc/urms/lnx/send_urms_data
 	fi
